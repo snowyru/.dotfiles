23a692cabb123698fc88c117614846e6d3c39817
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2024-04-27 11:21:05 +0200

    nvim and hypr

---
+++ hypr/scripts/slideshow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Infinite loop
+while true
+do
+    # Run the wallpaper.sh script
+    ~/.config/hypr/scripts/wallpaper.sh
+
+    # Wait for 15 minutes (900 seconds)
+    sleep 600
+done
---
+++ nvim/init.lua
@@ -0,0 +1,26 @@
+require("coconut");
+vim.cmd('colorscheme 2077');
+print("Lets gooooooooooo");
+vim.cmd('set wrap');
+vim.g.clipboard = { --this is just for wayland clipboard
+    name = 'WL-clipboard',
+    copy = {
+        ['+'] = 'wl-copy --foreground',
+        ['*'] = 'wl-copy --foreground --primary',
+    },
+    paste = {
+        ['+'] = 'wl-paste --no-newline',
+        ['*'] = 'wl-paste --no-newline --primary',
+    },
+    cache_enabled = 1,
+}
+
+require('tabnine').setup({ -- Activation and compatiblity config for tabnine
+  disable_auto_comment=true,
+  accept_keymap="<Tab>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = {gui = "#808080", cterm = 244},
+  exclude_filetypes = {"TelescopePrompt", "NvimTree"},
+  log_file_path = nil, -- absolute path to Tabnine log file
+})
